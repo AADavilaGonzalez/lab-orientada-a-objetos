cd72577f33d1ab0d67d67ffea6d6bbc30a78c1d4
Aldo Davila <aldoda2003gp5@gmail.com>
2025-09-14 04:50:17 -0600

    primer avance practica1

---
+++ practica1/HolaMundoAD.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class HolaMundoAD {
+    public static void main(String[] args) {
+        System.out.println(
+            "Aldo Adrian Davila Gonzalez\n"
+            + "Matricula: 1994122\n"
+            + LocalDate.now().toString()
+        );
+    }
+}
